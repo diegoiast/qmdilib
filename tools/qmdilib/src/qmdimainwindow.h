694b034c64b647b84d9c62be3d6858afd18f15a2
Diego Iastrubni <elcuco@kde.org>
2006-09-08 17:41:07 +0000

    i knew i forfot something...

---
+++ tools/qmdilib/src/qmdimainwindow.h
@@ -0,0 +1,14 @@
+#include "qmdihost.h"
+#include <QMainWindow>
+
+class QWidget;
+
+class qmdiMainWindow: public QMainWindow, public qmdiHost
+{
+public:	
+	qmdiMainWindow( QWidget * parent = 0, Qt::WindowFlags flags = 0 ):
+		QMainWindow( parent, flags )
+		{
+			// stub function
+		}
+};
