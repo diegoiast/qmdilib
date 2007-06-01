4a9211a31c51c46429e0b11ab941ef5222ac829d
Diego Iastrubni <elcuco@kde.org>
2007-06-01 19:20:59 +0000

    = New class = This is the same as QexTextEdit2, except it also will try to display help about the word under the cursor by asking the PluginManager to open "help:QApplication". If the plugin manager could not display help about this keyword, a message is displayed on the status bar of the main window.

---
+++ tools/qmdilib/demos/plugin-demo/plugins/editor/qexeditor2.cpp
@@ -0,0 +1,41 @@
+#include <QIcon>
+#include <QAction>
+#include <QTextCursor>
+#include <QAction>
+#include <QMainWindow>
+#include <QStatusBar>
+
+#include "qexeditor2.h"
+#include "pluginmanager.h"
+#include "qmdiserver.h"
+
+QexTextEdit2::QexTextEdit2( QString file, bool singleToolbar, QWidget *parent )
+	:QexTextEdit( file, singleToolbar, parent )
+{
+	actionShowQtKeyword = new QAction( QIcon(":images/copy.png"), tr("&Show keywork documentation"), this );
+	actionShowQtKeyword->setShortcut( tr("F1") );
+	menus["&Help"]->addAction( actionShowQtKeyword );
+	
+	connect( actionShowQtKeyword, SIGNAL(triggered()), this, SLOT(showQtKeyword()) );
+}
+
+void QexTextEdit2::showQtKeyword()
+{
+	if (!mdiServer)
+		return;
+	
+	if (!mdiServer->mdiHost)
+		return;
+	
+	PluginManager *m = dynamic_cast<PluginManager*>(mdiServer->mdiHost);
+	QMainWindow   *w = qobject_cast<QMainWindow*>(this->window());
+	
+	QTextCursor cursor = textCursor();
+	cursor.select(QTextCursor::WordUnderCursor);
+	if (!m->openFile( "help:" + cursor.selectedText().toLower() ))	
+	{
+		// lets inform the user
+		if (w)
+			w->statusBar()->showMessage( "Could not find help for this keyword", 5 * 1000 ); // this is msec
+	}
+}
---
+++ tools/qmdilib/demos/plugin-demo/plugins/editor/qexeditor2.h
@@ -0,0 +1,21 @@
+#ifndef __QEXEDITOR2_H__
+#define __QEXEDITOR2_H__
+
+
+#include "qexeditor.h"
+
+class QexTextEdit2: public QexTextEdit
+{
+	Q_OBJECT
+public:
+	QexTextEdit2( QString file="", bool singleToolbar = false, QWidget *parent=0 );
+
+public slots:
+	void showQtKeyword();
+
+private:
+	QAction *actionShowQtKeyword;
+};
+
+#endif // __QEXEDITOR2_H__
+
