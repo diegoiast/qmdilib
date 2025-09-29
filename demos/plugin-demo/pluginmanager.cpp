c6eab5a342e9662695b8929476303745615b2978
Diego Iastrubni <diegoiast@gmail.com>
2025-09-29 17:33:32 +0300

    PluginManager: save state when changing minimal mode
    Should help with https://github.com/diegoiast/qtedit4/issues/124

--- demos/plugin-demo/pluginmanager.cpp
+++ demos/plugin-demo/pluginmanager.cpp
@@ -1423,6 +1423,7 @@ void PluginManager::on_actionHideGUI_changed() {
     mergeClient(currentClient());
     updateGUI();
 
+    saveSettings();
     setUpdatesEnabled(true);
     emit minimizedModeChanged(actionHideGUI->isChecked());
 }
